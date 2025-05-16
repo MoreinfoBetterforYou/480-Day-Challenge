a5ef903fb79876da770a97362c2036ef7d4d675b
MoreinfoBetterforYou <muhammadhassanfarooq920523@gmail.com>
2025-05-16 18:18:01 +0500

    Practice Exercise 2.1

---
+++ Book/Practice Exercise 2.1/index.js
@@ -0,0 +1,7 @@
+let str1 = 'Laurence';
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+console.table([typeof str1, typeof str2, typeof val1, typeof val2, typeof myNum]);
