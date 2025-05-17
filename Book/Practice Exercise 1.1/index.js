b0bf414585ef165659685ae2db10dbbe3cde0fed
MoreinfoBetterforYou <muhammadhassanfarooq920523@gmail.com>
2025-05-17 10:01:17 +0500

    Writing the questions also

--- Book/Practice Exercise 1.1/index.js
+++ Book/Practice Exercise 1.1/index.js
@@ -1,2 +1,10 @@
+/* Working with the console:
+    1. Open the browser console, type 4 + 10, and press Enter. What do you see as
+    the response?
+    2. Use the console.log() syntax, placing a value within the rounded brackets.
+    Try entering your name with quotes around it (this is to indicate the fact that
+    it's a text string—we'll get to this in the next chapter).
+*/
+
 console.log(4+10);
 console.log("M. Hassan Farooq");
---
+++ Book/Practice Exercise 2.2/index.js
@@ -0,0 +1,14 @@
+/* Practice exercise 2.2
+        Create a variable for your name, another one for your age, and another one for
+        whether you can code JavaScript or not.
+        Log to the console the following sentence, where name, age and true/false are
+        variables:
+            Hello, my name is Maaike, I am 29 years old and I can code JavaScript:
+            true. 
+*/
+let creatorName = `M. Hassan Farooq`;
+let age = 15;
+let canWriteJS = true;
+
+let message = `Hello, my name is ${creatorName}, I am ${age} years old and I can code JavaScript: ${canWriteJS}`;
+console.log(message);
