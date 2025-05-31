e0747fc070380d866a51c2a96d3b2a953c5dbded
MoreinfoBetterforYou <muhammadhassanfarooq920523@gmail.com>
2025-05-31 19:55:30 +0500

    Chapter 3 - Project 2

---
+++ Book/Chapter Project 3/Project 2/index.js
@@ -0,0 +1,21 @@
+const storeItems = [
+    {
+        productName: "Shoe",
+        model: "Air Jordan", 
+        cost: 39.99,
+        quantity: 6
+    },
+    {
+        productName: "Hat",
+        model: "Beach Hat",
+        cost: 19.99,
+        quantity: 10
+    },
+    {
+        productName: "Gloves",
+        model: "Fingerless Gloves",
+        cost: 14.99,
+        quantity: 8
+    }
+]
+console.log(storeItems[2].quantity);
