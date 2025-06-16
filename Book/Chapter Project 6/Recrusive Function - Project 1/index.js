c8dd368f18e39494e2cbf456ed752190b888e148
MoreinfoBetterforYou <muhammadhassanfarooq920523@gmail.com>
2025-06-16 14:26:50 +0500

    Recrusive Function - Project 1 - Chapter 6

---
+++ Book/Chapter Project 6/Recrusive Function - Project 1/index.js
@@ -0,0 +1,14 @@
+/* 
+Create a recursive function
+    Create a recursive function that counts up to 10. Invoke the function with different
+    start numbers as the arguments that are passed into the function. The function
+    should run until the value is greater than 10.
+*/
+function countTo10(number) {
+    if (number > 10){
+        return;
+    }
+    console.log(number);
+    countTo10(number + 1);
+}
+countTo10(1);
