da0543aabd85a7684784307f85a44377d8b19385
MoreinfoBetterforYou <muhammadhassanfarooq920523@gmail.com>
2025-06-22 10:19:26 +0500

    Pratice Exercise 8.6

---
+++ Book/Practice Exercise 8/Practice Exercise 8.6/index.js
@@ -0,0 +1,26 @@
+/* 
+Practice exercise 8.6
+Experiment with the Math object with these steps:
+    1. Output the value of PI into the console using Math.
+    2. Using Math get the ceil() value of 5.7, get the floor() value of 5.7, get the
+    round value of 5.7, and output it into the console.
+    3. Output a random value into the console.
+    4. Use Math.floor() and Math.random() to get a number from 0 to 10.
+    5. Use Math.floor() and Math.random() to get a number from 1 to 10.
+    6. Use Math.floor() and Math.random() to get a number from 1 to 100.
+    7. Create a function to generate a random number using the parameters of min
+    and max. Run that function 100 times, returning into the console a random
+    number from 1 to 100 each time.
+*/
+console.log(Math.PI);
+console.table([(Math.ceil(5.7)), (Math.floor(5.7)), (Math.round(5.7))]);
+console.log(Math.random());
+console.log(Math.floor(Math.random() * (10 - 0) + 0));
+console.log(Math.floor(Math.random() * (10 - 1) + 1));
+console.log(Math.floor(Math.random() * (100 - 1) + 1));
+function generateRandomNumber (min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+for (let i = 1; i <= 100; i++){
+    console.log((generateRandomNumber(1, 100)));
+}
